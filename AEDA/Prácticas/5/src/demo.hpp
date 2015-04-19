e18f0c55095bb39b898fa00f4ce55362b110592d
Daniel Ramos <danielramosacosta@hotmail.com>
2015-04-19 23:44:39 +0000

    Se me olvida meter las cosas nuevas

---
+++ AEDA/Prácticas/5/src/demo.hpp
@@ -0,0 +1,17 @@
+#ifndef _DEMO_HPP_
+#define _DEMO_HPP_
+
+#include <iostream>
+#include <vector>
+
+#include "key.hpp"
+//#include "key_human.hpp"
+//#include "key_human_dni.hpp"
+//#include "human.hpp"
+//#include "algorithm.hpp"
+
+void cabecera_demo(int tamano, int error);
+void editar_tamano(unsigned& tamano, unsigned& error);
+void modo_demostracion(void);
+
+#endif
---
+++ AEDA/Prácticas/5/src/estadistica.hpp
@@ -0,0 +1,16 @@
+#ifndef _ESTADISTICA_HPP_
+#define _ESTADISTICA_HPP_
+
+
+#include <iostream>
+#include <vector>
+
+#include "key.hpp"
+#include "key_human.hpp"
+#include "key_human_dni.hpp"
+#include "human.hpp"
+#include "algorithm.hpp"
+
+void modo_estadistica(void);
+
+#endif
