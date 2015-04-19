ac0736c8431506307bb461b8ed3f21901e021446
Daniel Ramos <danielramosacosta@hotmail.com>
2015-04-19 17:12:49 +0000

    Se me olvido añadir los otros archivitos

---
+++ AEDA/Prácticas/5/src/estadistica.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+#include "key.hpp"
+#include "key_human.hpp"
+#include "key_human_dni.hpp"
+#include "human.hpp"
+#include "algorithm.hpp"
+
+void modo_estadistica(void)
+{
+	system("clear");
+	std::cout << "Foo" << std::endl;
+	std::cin.ignore().get();
+}
