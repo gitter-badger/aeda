4d8b8cf12b72ee217a153dc102e01e050216ea3f
Daniel Ramos <danielramosacosta@hotmail.com>
2015-04-19 17:10:06 +0000

    Separadas las dos secciones

--- AEDA/Prácticas/5/src/main.cpp
+++ AEDA/Prácticas/5/src/main.cpp
@@ -7,190 +7,43 @@
 #include "human.hpp"
 #include "algorithm.hpp"
 
-
-/*
-void menu(unsigned &modo, unsigned &algoritmo, unsigned &tamano, bool &salir);
-void get_modo(int indice);
-void get_algoritmo(int indice);
-void editar_modo(unsigned &modo);
-void editar_algor4itmo(unsigned &algoritmo);
-void editar_tamano(unsigned &tamano);
-void ejecutar_algoritmo(unsigned &modo, unsigned &algoritmo, unsigned &tamano);
-*/
-
-void modo_demostracion(void);
-void modo_estadistica(void);
-
 int main(void)
 {
-	system("clear");
-
 	char modo = 'f';
+	bool quedarse = true;
+	bool error = false;
 
-	while(modo == 'f'){
+	while(quedarse){
+		system("clear");
 
-		std::cout << "=======================" << std::endl;
-		std::cout << "|| Practica 5 | AEDA ||" << std::endl;
-		std::cout << "=======================" << std::endl;
-		std::cout << std::endl;
-		std::cout << "Elije el modo del programa:" << std::endl;
-		std::cout << " ·[D]emostracion" << std::endl;
-		std::cout << " ·[E]stadistica" << std::endl;
-		if(modo == '(')
-			std::cout << "Modo invalido, introduce otro" << std::endl;
+		std::cout << "=======================\n";
+		std::cout << "|| AEDA | Practica 5 ||\n";
+		std::cout << "=======================\n";
+		std::cout << "\n";
+		std::cout << "Elije el modo del programa:\n";
+		std::cout << " o) Dem[o]stracion\n";
+		std::cout << " a) Est[a]distica\n";
+		std::cout << " q) Salir\n";
+		
+		
+		if(error)
+			std::cout << "Modo invalido, introduce otro\n";
 		std::cout << "   >";
 
+		std::cout.flush();
+
 		std::cin >> modo;
-
-		if(modo == 'd' || modo == 'D')
-			modo_demostracion();
-		else if(modo == 'e' || modo == 'E')
-			modo_estadistica();
-		else{
-			modo = 'f';
-
-
-		}
-	}
-
-}
-
-void modo_demostracion(void)
-{
-	system("clear");
-	std::cout << "Este es el modo demostracion" << std::endl;
-}
-
-void modo_estadistica(void)
-{
-	system("clear");
-	std::cout << "Este es el modo estadistica" << std::endl;
-}
-
-
-/*
-
-void menu(unsigned &modo, unsigned &algoritmo, unsigned &tamano, bool &salir)
-{
-	system("clear");
-
-	int seleccion;
-
-	std::cout << "=======================" << std::endl;
-	std::cout << "|| Practica 5 | AEDA ||" << std::endl;
-	std::cout << "=======================" << std::endl;
-	std::cout << std::endl;
-	std::cout << "1. Modo: "; get_modo(modo);
-	std::cout << "2. Algoritmo: "; get_algoritmo(algoritmo);
-	std::cout << "3. Tamano: " << tamano << std::endl;
-	std::cout << "4. Ejecutar" << std::endl;
-	std::cout << "5. Salir" << std::endl;
-	std::cout << std::endl;
-	std::cout << "   >";
-
-	switch(seleccion){
-
-	case 1: std::cout << "Editando el modo" << std::endl; editar_modo(modo); break;
-	case 2: std::cout << "Editando el algoritmo" << std::endl; editar_algoritmo(algoritmo); break;
-	case 3: std::cout << "Editando el tamano" << std::endl; editar_tamano(tamano); break;
-	case 4: std::cout << "Ejecutando" << std::endl; break;
-	case 5: std::cout << "Saliendo" << std::endl; salir = true; break;
-	case 6: std::cout << "Opcion invalida..." << std::endl; break;
-	}
-	std::cin.ignore().get();
-}
-
-void get_modo(int indice)
-{
-	switch(indice){
-	case 1: std::cout << "Demostracion" << std::endl; break;
-	case 2: std::cout << "Estadistica" << std::endl; break;
-	default: std::cout << "DESCONOCIDO" << std::endl; break;
-	}
-}
-
-void get_algoritmo(int indice)
-{
-	switch(indice){
-	case 1: std::cout << "Insercion" << std::endl; break;
-	case 2: std::cout << "Selección" << std::endl; break;
-	case 3: std::cout << "Shellsort" << std::endl; break;
-	case 4: std::cout << "QuickSort" << std::endl; break;
-	case 5: std::cout << "MergeSort" << std::endl; break;
-	default: std::cout << "DESCONOCIDO" << std::endl; break;
-	}
-}
-
-void editar_modo(unsigned &modo)
-{
-	modo = 999;
-	std::cout << "1. Modo demostracion" << std::endl;
-	std::cout << "2. Modo estadistica" << std::endl;
-
-	while(modo == 999)
-	{
-		std::cout << "   >";
-		std::cin >> modo;
+		
+		error = false;
+		
 		switch(modo){
-		case 1:
-		case 2: std::cout << "Introducido correctamente" << std::endl; break;
-		default: modo = 999; std::cout << "Eleccion erronea, introduce otra" << std::endl;
+			case 'o':
+			case 'O': modo_demostracion(); break;
+			case 'a':
+			case 'A': modo_estadistica(); break;
+			case 'q':
+			case 'Q': quedarse = false;
+			default: error = true;
 		}
 	}
 }
-
-void editar_algoritmo(unsigned &algoritmo)
-{
-	algoritmo = 999;
-	std::cout << "1. Insercion" << std::endl;
-	std::cout << "2. Seleccion" << std::endl;
-	std::cout << "3. Shellsort" << std::endl;
-	std::cout << "4. QuickSort" << std::endl;
-	std::cout << "5. MergeSort" << std::endl;
-
-	while(algoritmo == 999)
-	{
-		std::cout << "   >";
-		std::cin >> algoritmo;
-		switch(algoritmo){
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-		case 5: std::cout << "Introducido correctamente" << std::endl; break;
-		default: algoritmo = 999; std::cout << "Eleccion erronea, introduce otra" << std::endl;
-		}
-	}
-}
-
-void editar_tamano(unsigned &tamano)
-{
-	std::cout << "   >";
-	std::cin >> tamano;
-	std::cout << "Introducido correctamente" << std::endl;
-}
-
-void ejecutar_algoritmo(unsigned &modo, unsigned &algoritmo, unsigned &tamano)
-{
-
-}
-*/
-
-/*Manera de meter las cosas
-
-	std::vector<dra::key*> test;
-
-	for(int i = 0; i < 5; i++){
-		dra::key* dummy2 = new dra::key_human_dni(new dra::human);
-		test.push_back(dummy2);
-	}
-
-	for(int i = 0; i < 5; i++){
-		std::cout << test[i] << std::endl;
-	}
-
-	for(int i = 0; i < 5; i++){
-		delete test[i];
-	}
-
-*/
