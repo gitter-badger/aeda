f9666dad6ab72700c883da965ddccbd95a6b6cfc
Daniel Ramos <danielramosacosta@hotmail.com>
2015-04-21 15:46:56 +0100

    Empezando la Practica 6

---
+++ AEDA/Prácticas/6/PR6/binarynode.h
@@ -0,0 +1,111 @@
+#ifndef BINARYNODE
+#define BINARYNODE
+
+namespace dra{
+
+template<class T>
+class binaryNode{
+private:
+	T data_;
+	binaryNode* left_;
+	binaryNode* right_;
+public:
+	binaryNode(void);
+	binaryNode(const T& data);
+	~binaryNode(void);
+
+	binaryNode*& left(void);
+	binaryNode* left(void) const;
+
+	binaryNode*& right(void);
+	binaryNode* right(void) const;
+
+	T& data(void);
+	T data(void) const;
+
+	binaryNode<T>& operator=(const binaryNode<T>&);
+	std::ostream& toStream(std::ostream& os) const;
+};
+
+
+template<class T>
+binaryNode<T>::binaryNode(void):
+left_(nullptr),
+right_(nullptr),
+data_()
+{}
+
+template<class T>
+binaryNode<T>::binaryNode(const T& data):
+left_(nullptr),
+right_(nullptr),
+data_(data)
+{}
+
+template<class T>
+binaryNode<T>::~binaryNode(void)
+{}
+
+template<class T>
+binaryNode<T>*& binaryNode<T>::left(void)
+{
+	return left_;
+}
+
+template<class T>
+binaryNode<T>* binaryNode<T>::left(void) const
+{
+	return left_;
+}
+
+template<class T>
+binaryNode<T>*& binaryNode<T>::right(void)
+{
+	return right_;
+}
+
+template<class T>
+binaryNode<T>* binaryNode<T>::right(void)
+{
+	return right_;
+}
+
+template<class T>
+T& binaryNode<T>::binaryNode(void)
+{
+	return data_;
+}
+
+template<class T>
+T binaryNode<T>::binaryNode(void) const
+{
+	return data_;
+}
+
+
+template<class T>
+std::ostream& binaryNode<T>::toStream(std::ostream& os) const
+{
+	if(right_ != nullptr)
+		os << "[ " << data_ << " ][" << right_ << "]->";
+	else
+		os << "[ " << data_ << " ][nullptr]";
+
+	if(left_ != nullptr)
+		std::cout << "|\nv\n" << left_;
+	return os;
+}
+
+}
+
+//==============================================================================
+// Fin de implementación de la clase
+//==============================================================================
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const dra::binaryNode<T>& n)
+{
+	return n.toStream(os);
+}
+
+#endif // BINARYNODE
---
+++ AEDA/Prácticas/6/PR6/binarytree.h
@@ -0,0 +1,19 @@
+#ifndef BINARYTREE
+#define BINARYTREE
+
+#include "binarynode.h"
+
+namespace dra{
+
+
+
+template<class T>
+class binaryTree{
+private:
+	binaryNode* root;
+};
+
+}
+
+#endif // BINARYTREE
+
